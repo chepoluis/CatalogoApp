d52316f034af548339757307a2b5aec2f2b3d877
Luis <vsjoseluisvs@gmail.com>
2018-09-16 15:53:09 -0500

    Initial commit

---
+++ app/src/main/java/app/catalogo/com/catalogoapp/MainActivity.java
@@ -0,0 +1,13 @@
+package app.catalogo.com.catalogoapp;
+
+        import android.support.v7.app.AppCompatActivity;
+        import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
